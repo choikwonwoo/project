7694899deb8780bc5221ea4856d2ccf9c537525b
choikwonwoo <65008329+choikwonwoo@users.noreply.github.com>
2021-03-31 14:51:41 +0900

    Add files via upload

--- testst.py
+++ testst.py
@@ -1,44 +1,69 @@
 import sys
-from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5 import QtCore
 from PyQt5.QtGui import *
 from PyQt5 import uic
-import cv2
-from pyzbar import pyzbar
+import serial
 import qrcode
-import threading
+import queue
 
-uiadresss =uic.loadUiType('minsoo.ui')[0]
+
+uiadresss = uic.loadUiType('minsoo.ui')[0]
 Temps = []
 Data = []
+rawBarcode = ''
+q = queue.Queue()
+print(q)
+class Thread1(QThread):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+    def run(self):
+        while 1:
+            ser = serial.Serial('/dev/ttyUSB0', 9600)
+            rawBarcode = str(ser.read(6))
+            rawBarcode = rawBarcode.replace("b","").replace("'","")
+            print(rawBarcode)
+            q.put(rawBarcode)
+            print(q)
+            # mainwindow.Make_Temp()        
+
 class CalWindow(QDialog):
-    def __init__(self,parent):
+    def __init__(self, parent):
+
         super(CalWindow, self).__init__(parent)
-        self.a= uic.loadUi("C:\\Users\\Assistant_1\\anaconda3\\Lib\\site-packages\\PyQt5\\uic\\minsoo2.ui",self)
+
+
+        self.a = uic.loadUi("C:\\Users\\Assistant_1\\anaconda3\\Lib\\site-packages\\PyQt5\\uic\\minsoo2.ui", self)
         self.QR_image()
         self.a.show()
         self.pushButton.clicked.connect(self.exit)
 
     def QR_image(self):
-        self.lbl =QLabel(self)
-        self.lbl.move(150,-150)
-        self.lbl.resize(400,400)
+        self.lbl = QLabel(self)
+        self.lbl.move(150, -150)
+        self.lbl.resize(400, 400)
         pixmap = QPixmap("qr_test.png")
         pixmap = pixmap.scaledToWidth(100)
         self.lbl.setPixmap(QPixmap(pixmap))
-        self.resize(400,400)
-        
+        self.resize(400, 400)
 
     def exit(self):
         self.a.close()
 
-class Main_Window(QMainWindow,uiadresss):
+
+class Main_Window(QMainWindow, uiadresss):
 
     def __init__(self):
         super().__init__()
-        
-        
+        x = Thread1()
+        x.start()
         self.setupUi(self)
+
+    
+        self.Make_Temp()
+
+        
         
         self.pushButton.clicked.connect(self.newWindow)
 
@@ -52,95 +77,74 @@ class Main_Window(QMainWindow,uiadresss):
             del self.count[row]
             self.tableWidget.removeRow(row)
             self.nowPrice()
-           
-
 
     def plus(self):
-        
+
         self.item = QPushButton("del")
         self.item.clicked.connect(self.del_botton)
         self.tableWidget.setRowCount(len(Temps))
-        self.tableWidget.setCellWidget(len(Temps)-1,3,self.item)
+        self.tableWidget.setCellWidget(len(Temps) - 1, 3, self.item)
 
         self.item1 = QPushButton("+")
         self.item2 = QPushButton("-")
-        
 
         self.item1.clicked.connect(self.countPlus)
         self.item2.clicked.connect(self.countMinus)
-        self.tableWidget.setCellWidget(len(Temps)-1,4,self.item1)
-        self.tableWidget.setCellWidget(len(Temps)-1,5,self.item2)
-        
-        
-        
+        self.tableWidget.setCellWidget(len(Temps) - 1, 4, self.item1)
+        self.tableWidget.setCellWidget(len(Temps) - 1, 5, self.item2)
 
     def countPlus(self):
         button = self.sender()
         if button:
-            row = self.tableWidget.indexAt(button.pos()).row() 
-            self.count[row] = str(int(self.count[row]) +1)
+            row = self.tableWidget.indexAt(button.pos()).row()
+            self.count[row] = str(int(self.count[row]) + 1)
             item = QTableWidgetItem(self.count[row])
             self.tableWidget.setItem(row, 2, item)
             self.nowPrice()
-
 
     def countMinus(self):
         button = self.sender()
         if button:
             row = self.tableWidget.indexAt(button.pos()).row()
             if int(self.count[row]) != 1:
-                self.count[row] = str(int(self.count[row]) -1)
+                self.count[row] = str(int(self.count[row]) - 1)
                 item = QTableWidgetItem(self.count[row])
                 self.tableWidget.setItem(row, 2, item)
                 self.nowPrice()
             else:
                 pass
-            
 
- 
-
-    def listMaker(self):    
+    def listMaker(self):
         self.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
         self.subject = len(self.name)
-       
-        
-        allsubjectData = [self.name1 , self.price1, self.count]
+
+        allsubjectData = [self.name1, self.price1, self.count]
 
         for i in range(self.subject):
             for j in range(3):
                 item = QTableWidgetItem(allsubjectData[j][i])
                 item.setTextAlignment(QtCore.Qt.AlignVCenter | QtCore.Qt.AlignHCenter)
                 self.tableWidget.setItem(i, j, item)
-    
 
-    def read_barcodes(self,frame):
-        barcodes = pyzbar.decode(self.frame)
-        for barcode in barcodes:
-            x, y, w, h = barcode.rect
-            # 1
-            self.barcode_info = barcode.data.decode('utf-8')
-            cv2.rectangle(self.frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-            # 2
-            font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(self.frame, self.barcode_info, (x + 6, y - 6), font, 2.0, (255, 255, 255), 1)
-            self.Make_Temp(self.barcode_info)
-        return self.frame
-
-    def Make_Temp(self,barcode_info):
+    def Make_Temp(self):
         
-        if self.barcode_info in Temps:
+        rawBarcode =q.get()
+        if rawBarcode in Temps:
             print("```")
         else:
-            Temps.append(self.barcode_info)
+            Temps.append(rawBarcode)
+            print(Temps)
             self.plus()
+        # Temps.append('1:20:1')
+        # self.plus()
 
         self.name = [0 for i in range(len(Temps))]
         self.name1 = [0 for i in range(len(Temps))]
         self.price = [0 for i in range(len(Temps))]
         self.price1 = [0 for i in range(len(Temps))]
         self.count = [0 for i in range(len(Temps))]
-        
+
         print(Temps)
         Data = [0 for i in range(len(Temps))]
         for i in range(len(Temps)):
@@ -151,40 +155,40 @@ class Main_Window(QMainWindow,uiadresss):
             self.name[i] = Data[i][0]
             self.price[i] = Data[i][1]
             self.count[i] = Data[i][2]
-            
+
         print(self.name)
         print(self.price)
         print(self.count)
-        
+
         for i in range(len(self.name)):
             if self.name[i] == "1":
-                self.name1[i] = '감자'
+                self.name1[i] = 'DDD'
             elif self.name[i] == "2":
-                self.name1[i] = '고구마'
+                self.name1[i] = 'D'
             elif self.name[i] == "3":
-                self.name1[i] = '과자'
+                self.name1[i] = 'A'
             elif self.name[i] == "4":
-                self.name1[i] = '음료'
+                self.name1[i] = 'C'
             elif self.name[i] == "5":
-                self.name1[i] = '물'
+                self.name1[i] = 'Q'
             elif self.name[i] == "6":
-                self.name1[i] = '라면'
+                self.name1[i] = 'W'
             elif self.name[i] == "7":
-                self.name1[i] = '우유'
+                self.name1[i] = 'E'
             elif self.name[i] == "8":
-                self.name1[i] = '치즈'
+                self.name1[i] = 'R'
             elif self.name[i] == "9":
-                self.name1[i] = '아이스크림'
+                self.name1[i] = 'T'
             elif self.name[i] == "10":
-                self.name1[i] = '버섯'
+                self.name1[i] = 'Y'
             elif self.name[i] == "11":
-                self.name1[i] = '냉동식품'
+                self.name1[i] = 'U'
             elif self.name[i] == "12":
-                self.name1[i] = '생활용품'
+                self.name1[i] = 'I'
             elif self.name[i] == "13":
-                self.name1[i] = '전자제품'
+                self.name1[i] = 'O'
 
-            self.price1[i] = str(int(self.price[i])*100)
+            self.price1[i] = str(int(self.price[i]) * 100)
             self.nowPrice()
             self.listMaker()
             print(self.price)
@@ -192,52 +196,37 @@ class Main_Window(QMainWindow,uiadresss):
             print(self.count)
 
 
-
     def nowPrice(self):
         totalPrice = 0
         for i in range(len(self.price)):
-            totalPrice += int(self.price1[i])*int(self.count[i])
-        self.textEdit.setText(str(totalPrice)+'원')
- 
+            totalPrice += int(self.price1[i]) * int(self.count[i])
+        self.textEdit.setText(str(totalPrice) + '원')
 
     def newWindow(self):
         sumString = ""
         for i in range(len(self.name)):
-            sumString += self.name[i] +':'+ self.price[i] +':'+ self.count[i]
-            if i != int(len(self.name))-1:
+            sumString += self.name[i] + ':' + self.price[i] + ':' + self.count[i]
+            if i != int(len(self.name)) - 1:
                 sumString += ':'
 
         img1 = qrcode.make(sumString)
         img1.save('qr_test.png')
         CalWindow(self)
-        
 
-    def main(self):
-    #1
-        
-        camera = cv2.VideoCapture(0)
-        ret, self.frame = camera.read()
-    #2
-        while ret:
-            ret, self.frame = camera.read()
-            self.frame = self.read_barcodes(self.frame)
-            cv2.imshow('Barcode/QR code reader', self.frame)
-            
-            if cv2.waitKey(1) & 0xFF == 27:
-                break
-
-    #3
-        camera.release()
-        cv2.destroyAllWindows()
-
-
-
+    # def main(self):
+    #     while 1:
+    #         ser = serial.Serial('/dev/ttyUSB0', 9600)
+    #         line = ser.read(6)
+    #         print(line)
+    #         self.Make_Temp(line)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     mainwindow = Main_Window()
-    t =threading.Thread(target =mainwindow.show())
-    t.start()
-    mainwindow.main()
+
+    mainwindow.show()
+    #mainwindow.show()
+
+    
 
     app.exec_()
